e308e7b100fc2ea8346eab5407df4f7467194b33
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2025-02-15 12:08:14 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,41 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminLogin from './pages/AdminLogin';
+import { useState, useEffect } from 'react';
+import { supabase } from './lib/supabase';
+
+function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession()
+      .then(({ data: { session } }) => {
+        setSession(session)
+      })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <Router>
+      
+        
+          
+            
+              {session ? (
+                <AdminDashboard session={session} />
+              ) : (
+                <AdminLogin />
+              )}
+            
+          
+        
+      
+    </Router>
+  );
+}
+
+export default App;
